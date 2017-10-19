537541f84be4f60e08a48b976fd924d2e3a48dcc
duanhaochen <duanhaochen@126.com>
2017-10-19 14:28:50 +0800

    修改微打赏监听算法

---
+++ app/webWorker/function/computingWds.js
@@ -0,0 +1,94 @@
+/**
+ * 微打赏计算
+ */
+import cheerio from 'cheerio';
+
+/* 将html抽象成聚聚榜 */
+/*
+<li class="line1px">
+  <span class="suport_ran">21</span>
+  <a href="otuser_tmp?user_id=123">
+    <span class="avatar">
+	    <img class="lazyload" data-original="" src="" onerror="">
+	  </span>
+    <span class="nickname\">昵称</span>
+    <span class="money">￥ 400.00</span>
+  </a>
+</li>
+*/
+export function juju(xmlStr: string): { allMount: number, arr: Array, obj: Object }{
+  const xml: any = cheerio(`<ul id="xml">${ xmlStr }</ul>`);
+  const arr: Array = [];
+  const obj: Object = {};
+  let allMount: number = 0;
+  xml("#xml li").each((index: number, item: any): void=>{
+    const id: string = (item.find('a').eq(0).attr('href')).match(/[0-9]+/)[0];     // 用户id
+    const nickname: string = item.find('.nickname').text();                        // 用户的昵称
+    const money: Number = Number(item.find('.money').text().match(/[0-9.]+/)[0]);  // 用户的打赏金额
+    const o: Object = {
+      index,
+      id,
+      nickname,
+      money
+    };
+    obj[id] = o;         // 无序
+    arr.push(o);         // 有序
+    allMount += money;   // 总金额
+  });
+  return {
+    obj,
+    arr,
+    allMount
+  };
+}
+
+/* 将html抽象成打卡榜 */
+/*
+<li class="line1px">
+  <span class="suport_ran">1</span>
+  <a href="otuser_tmp?user_id=123">
+    <span class="avatar">
+      <img class="lazyload" data-original="" src="" onerror="">
+    </span>
+    <span class="nickname">昵称</span>
+    <span class="money">3天</span>
+  </a>
+</li>
+*/
+export function daka(xmlStr: string): Array{
+  const xml: any = cheerio(`<ul id="xml">${ xmlStr }</ul>`);
+  const arr: Array = [];
+  xml("#xml li").each((index: number, item: any): void=>{
+    const id: string = (item.find('a').eq(0).attr('href')).match(/[0-9]+/)[0];     // 用户id
+    const nickname: string = item.find('.nickname').text();                        // 用户的昵称
+    const day: Number = Number(item.find('.money').text().match(/[0-9]+/)[0]);     // 打赏天数
+    arr.push({
+      index,
+      id,
+      nickname,
+      day
+    });
+  });
+  return arr;
+}
+
+/* 计算变化成员 */
+export function changeMembers(oldObj: Object, newArr: Array, from: number, to: number): Array{
+  if(newArr.length === 0){
+    return [];
+  }
+  // 判断集资是否变化
+  if(from === to){
+    const id: string = newArr[from].id;
+    if((!(id in oldObj)) || (newArr[from].money !== oldObj[id].money)){
+      return [newArr[from]];
+    }else{
+      return [];
+    }
+  }
+
+  const middle: number = Math.floor((to - from) / 2) + from;
+  const left: Array = changeMembers(oldObj, newArr, from, middle);
+  const right: Array = changeMembers(oldObj, newArr, middle + 1, to);
+  return left.concat(right);
+}
--- app/webWorker/weiDaShang.js
+++ app/webWorker/weiDaShang.js
@@ -2,38 +2,22 @@
 /**
  * 微打赏轮询
  *
- * 评论榜
- * [POST]https://wds.modian.com/ajax_comment
- * pageNum
- * moxi_id
- * pro_id
- * response:
- *   reply_fuid   用户id
- *   pay_amount   当前打卡数
- *   ctime        时间戳
- *   c_userinfo
- *     nickname   用户的昵称
- *
  * 排行榜
- * [POST] https://wds.modian.com/ajax_backer_list
+ * [POST] https://wds.modian.com/ajax/backer_ranking_list
  * pro_id  : 微打赏id
  * type    : 1 聚聚榜 2 打卡榜
  * page    : 页数
- * pageSize: 每次请求的数据数
- * response:
- *   user_id           用户id
- *   total_back_amount 总打卡数
+ * page_size: 每次请求的数据数
  */
 import getData from './function/getData';
+import { juju, daka, changeMembers } from './function/computingWds';
 
-const listUrl: string = `https://wds.modian.com/ajax_backer_list`;
-const commentUrl: string = `https://wds.modian.com/ajax_comment`;
+const listUrl: string = `https://wds.modian.com/ajax/backer_ranking_list`;
 let wdsId: ?string = null;
 let moxiId: ?string = null;
 let title: ?string = null;
 let timer: ?number = null;     // 轮询定时器
 let oldData: ?Array = null;    // 旧数据
-let oldComment: ?Array = null; // 旧评论榜
 
 addEventListener('message', function(event: Object): boolean{
   const { data }: { data: Object } = event;
@@ -44,25 +28,18 @@ addEventListener('message', function(event: Object): boolean{
     title = data.title;
 
     // 初始化
-    Promise.all([
-      getData('POST', listUrl, `pro_id=${ wdsId }&type=1&page=1&pageSize=10000000000`),
-      getData('POST', commentUrl, `pageNum=1&moxi_id=${ moxiId }&pro_id=${ wdsId }`)
-    ]).then((result: Array<Object>)=>{
-      const [data1, data2]: [Object, Object] = result;
+    getData('POST', listUrl, `pro_id=${ wdsId }&type=1&page=1&page_ize=10000000000`).then((result: Object)=>{
       // 获取旧排行榜
-      if(data1.status === '-1'){
-        oldData = [];
+      if(result.status !== 0){
+        oldData = {
+          obj: {},
+          arr: [],
+          allMount: 0
+        };
       }else{
-        oldData = data1.data;
-      }
-      // 获取旧评论榜
-      if(data2.status === '-1'){
-        oldComment = [];
-      }else{
-        oldComment = data2.des;
+        oldData = juju(result.data.html);
       }
       // 开启轮询
-      polling();
       timer = setInterval(polling, 5000);
     });
     return true;
@@ -80,55 +57,32 @@ addEventListener('message', function(event: Object): boolean{
 /* 轮询事件 */
 async function polling(): void{
   try{
-    const ct: Object = await getData('POST', commentUrl, `pageNum=1&moxi_id=${ moxiId }&pro_id=${ wdsId }`);
-    // 等于-1时报错
-    if(ct.status !== '-1'){
-      const { des }: { des: Array } = ct;
-      let newData: Array = [];
-      if(oldData.length === 0){
-        // 无旧数据，不需要对比时间戳，只需要去掉评论
-        for(let i: number = 0, j: number = des.length; i < j; i++){
-          if(des[i].pay_amount !== ''){
-            newData.push(des[i]);
-          }
-        }
-      }else{
-        // 有旧数据时，对比时间戳
-        for(let i: number = 0, j: number = des.length; i < j; i++){
-          if(des[i].ctime !== oldComment[0].ctime){
-            if(des[i].pay_amount !== ''){
-              newData.push(des[i]);
-            }
-          }else{
-            break;
-          }
-        }
-      }
-      // 计算打赏金额和排名，newData数组的长度为0时集资不变
-      if(newData.length > 0){
-        // 获取新排名
-        const bl: Object = await getData('POST', listUrl, `pro_id=${ wdsId }&type=1&page=1&pageSize=10000000000`);
-        const len1: number = bl.data.length - 1;
-        const amt: String = String((allMount(bl.data, 0, len1)).toFixed(2));  // 当前的总集资
+    const ct: Object = await getData('POST', listUrl, `pro_id=${ wdsId }&type=1&page=1&page_ize=10000000000`);
+    // 不等于0时报错
+    if(ct.status !== 0){
+      const newD = juju(ct.data.html);
+      if(newD.allMount !== oldData.allMount){
+        const newData: Array = changeMembers(oldData.obj, newD.arr, 0, newD.arr.length - 1);
+        // 计算打赏金额和排名
         const jizi: Array = [];
         for(let i: Object = 0, j: Object = newData.length; i < j; i++){
           const item: Object = newData[i];
-          const user_id: string = item.reply_fuid;                                                  // 当前用户的id
-          const oldIndex: ?number = indexOf(oldData, user_id, 0, oldData.length - 1);               // 旧排名
-          const newIndex: ?number = indexOf(bl.data, user_id, 0, len1);                             // 新排名
+          const user_id: string = item.id;                                                          // 当前用户的id
+          const oldIndex: ?number = user_id in oldData.obj ? oldData.obj[user_id].index : null;     // 旧排名
+          const newIndex: ?number = item.index;                                                     // 新排名
           const promote: number = oldIndex ? oldIndex - newIndex + 1 : oldData.length - newIndex;   // 排名提升
+          const pay_amount: number = item.money - oldData.obj[user_id].money;                       // 打赏金额
           jizi.push({
             user_id,
-            pay_amount: item.pay_amount,          // 打赏金额
-            nickname: item.c_userinfo.nickname,   // 用户昵称
+            pay_amount,                    // 打赏金额
+            nickname: item.nickname,       // 用户昵称
             newIndex,
             promote: promote < 0 ? 0 : promote,
-            allMount: amt
+            allMount: newD.allMount
           });
         }
         // 更新旧对比数据
-        oldData = bl.data;
-        oldComment = ct.des;
+        oldData = newD;
         // 将数据发送回主线程
         postMessage({
           type: 'change',
@@ -139,46 +93,4 @@ async function polling(): void{
   }catch(err){
     console.log(err);
   }
-}
-
-/* 计算总集资数 */
-function allMount(rawArray: Array, from: number, to: number): number{
-  if(rawArray.length === 0){
-    return 0;
-  }
-  // from === to时，返回获取到的集资数
-  if(from === to){
-    return Number(rawArray[from]['total_back_amount']);
-  }
-  // 拆分计算
-  const middle: number = Math.floor((to - from) / 2) + from;
-  const left: number = allMount(rawArray, from, middle);
-  const right: number = allMount(rawArray, middle + 1, to);
-  return left + right;
-}
-
-/* 查找索引，没有则返回null */
-function indexOf(rawArray: Array, value: string, from: number, to: number): ?number{
-  if(rawArray.length === 0){
-    return null;
-  }
-  // from === to时，判断是否匹配
-  if(from === to){
-    if(rawArray[from].user_id === value){
-      return from;
-    }else{
-      return null;
-    }
-  }
-  // 拆分计算
-  const middle: number = Math.floor((to - from) / 2) + from;
-  const left: number = indexOf(rawArray, value, from, middle);
-  if(left !== null){
-    return left;
-  }
-  const right: number =indexOf(rawArray, value, middle + 1, to);
-  if(right !== null){
-    return right;
-  }
-  return null;
 }
