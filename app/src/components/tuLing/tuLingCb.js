a96140123c885339d19e98b85029a93de2264018
duanhaochen <duanhaochen@126.com>
2017-10-19 23:32:43 +0800

    修改机器人回答

--- app/src/components/tuLing/tuLingCb.js
+++ app/src/components/tuLing/tuLingCb.js
@@ -13,15 +13,11 @@ function getTuLing(command: string[], qq: SmartQQ): void{
     },
     dataType: 'json',
     success: function(data: string, status: number, xhr: XMLHttpRequest): void{
-      if(data.code === 100000){
-        let text: string = `${ data.text }`;
-        if('url' in data){
-          text += `\n${ data.url }`;
-        }
-        qq.sendFormatMessage(text);
-      }else{
-        qq.sendMessage(`[ERROR] ${ data.code }: ${ data.text }`);
+      let text: string = `${ data.text }`;
+      if('url' in data){
+        text += `\n${ data.url }`;
       }
+      qq.sendFormatMessage(text);
     },
     error: function(err: any): void{
       qq.sendMessage('[ERROR] 机器人返回数据失败。');
