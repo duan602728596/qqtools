734892a5ad3e7ad2f43d286e0d3ce8838412919b
duanhaochen <duanhaochen@126.com>
2017-10-19 09:43:32 +0800

    优化算法

--- app/webWorker/minfo.js
+++ app/webWorker/minfo.js
@@ -3,23 +3,43 @@
  * 根据uin查重
  */
 
-function array2obj(rawArray: Array): Object{
-  const o: Object = {};
-  rawArray.forEach((item: Object, index: number): void=>{
-    o[item.uin] = item;
-  });
-  return o;
+function objMerge(obj1: Object, obj2: Object): Object{
+  for(const key: string in obj1){
+    obj2[key] = obj1[key];
+  }
+  return obj2;
+}
+
+function array2obj(rawArray: Array, from: number, to: number): Object{
+  const len: number = rawArray.length;
+  if(len === 1){
+    return rawArray[from];
+  }
+
+  if(len === 2){
+    return objMerge(rawArray[from], rawArray[to]);
+  }
+
+  const middle: number = Math.floor((to - from) / 2) + from;
+  const left: Object = array2obj(rawArray, from, middle);
+  const right: Object = array2obj(rawArray, middle + 1, to);
+  return array2obj(left, right);
 }
 
 /* 查询是否在旧群中，并返回一个数组 */
-function getNewMinfo(oldData: Object, newData: Object): Array{
-  const arr: Array = [];
-  for(const key: string in newData){
-    if(!(key in oldData)){
-      arr.push(newData[key]);
+function getNewMinfo(oldData: Object, newList: Array, from: number, to: number): Array{
+  if(from === to){
+    if(newList[from].uin in oldData){
+      return newList[from];
+    }else{
+      return [];
     }
   }
-  return arr;
+
+  const middle: number = Math.floor((to - from) / 2) + from;
+  const left: Array = getNewMinfo(oldData, newList, from, middle);
+  const right: Array = getNewMinfo(oldData, newList, middle + 1, to);
+  return left.concat(right);
 }
 
 addEventListener('message', function(event: Object): void{
@@ -28,10 +48,8 @@ addEventListener('message', function(event: Object): void{
     newList: Array
   } = event.data;
 
-  const oldObj: Object = array2obj(oldList);
-  const newObj: Object = array2obj(newList);
-
-  const minfo: Array = getNewMinfo(oldObj, newObj);
+  const oldObj: Object = oldList.length > 0 ? array2obj(oldList) : {};
+  const minfo: Array = newList.length > 0 ? getNewMinfo(oldObj, newList, 0, newList.length - 1) : [];
 
   postMessage({
     minfo
