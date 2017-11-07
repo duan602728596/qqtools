f6323e93ce2e381db36b9f3941e0d1bb07c90c74
duanhaochen <duanhaochen@126.com>
2017-11-07 18:20:25 +0800

    修改help命令

--- app/src/components/help/helpCb.js
+++ app/src/components/help/helpCb.js
@@ -5,8 +5,8 @@ async function helpCb(qq: SmartQQ): void{
 
   let text1: string = `【帮助】
  · [微打赏 或 wds]：查看当前微打赏链接
- · [(微打赏 或 wds) 1 ?number]：查看当前的聚聚榜
- · [(微打赏 或 wds) 2 ?number]：查看当前的打卡榜
+ · [(微打赏 或 wds) 1 ?number]：查看聚聚榜
+ · [(微打赏 或 wds) 2 ?number]：查看打卡榜
  · [直播列表 或 zb]：查看当前的口袋48直播列表
  · [(天气预报 或 tq) 城市]：查询天气情况
  · [say 你想说的话]：机器人
