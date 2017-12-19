8fcf0ef0b1f2346ac6518829e2cf170dc83da040
duanhaochen <duanhaochen@126.com>
2017-12-19 12:42:23 +0800

    修改了ajax函数的解析json的问题

--- app/src/components/modian/function/getData.js
+++ app/src/components/modian/function/getData.js
@@ -6,13 +6,9 @@ function getData(method: string, url: string, data: string){
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.setRequestHeader('Cache-Control', 'no-cache');
     xhr.addEventListener('readystatechange', function(event: Event): void{
-      if(xhr.status === 200){
-        try{
-          const res: JSON = JSON.parse(xhr.response);
-          resolve(res);
-        }catch(err){
-          // ！获取到的json虽然能够正常解析，但是会报格式错误，所以使用try来避免输出错误信息
-        }
+      if(xhr.status === 200 && xhr.readyState === 4){
+        const res: JSON = JSON.parse(xhr.response);
+        resolve(res);
       }
     });
     xhr.send(data);
