7351948bf6aee92c654564beb27ba0c565514c23
duanhaochen <duanhaochen@126.com>
2018-04-30 21:12:20 +0800

    更新配置

--- app/config/babel.config.js
+++ app/config/babel.config.js
@@ -7,9 +7,14 @@ module.exports = {
     cacheDirectory: path.join(__dirname, '../.babelCache'),
     presets: ['@babel/preset-flow', '@babel/preset-react'],
     plugins: [
-      '@babel/plugin-proposal-decorators',
-      '@babel/plugin-proposal-export-default-from',
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true
+        }
+      ],
       '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-export-default-from',
       [
         'import',
         {
