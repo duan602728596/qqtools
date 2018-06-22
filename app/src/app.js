39e7ee153091ecd2a6e02b186eccdf258f5cf727
段昊辰 <duanhaochen@126.com>
2018-06-22 12:22:09 +0800

    调整目录结构

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Routers from './router/routers';
+import Routers from './router/Routers';
 import { LocaleProvider } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 import store from './store/store';
