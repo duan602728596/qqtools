bc568a295d7f0e60c33da889098d9f9266d87c5b
段昊辰 <duanhaochen@126.com>
2018-10-13 00:08:59 +0800

    调整集资输出模板

--- app/src/modules/Option/Edit/utils/initialValues.js
+++ app/src/modules/Option/Edit/utils/initialValues.js
@@ -4,7 +4,7 @@ export const modianUrlTemplate: string = '摩点：{{ modianname }}\nhttps://m.m
 export const modianTemplate: string = '@{{ id }} 刚刚在【{{ modianname }}】打赏了{{ money }}元，'
   + '感谢这位聚聚！\n摩点项目地址：https://m.modian.com/project/{{ modianid }}.html\n'
   + '当前进度：￥{{ alreadyraised }} / ￥{{ goal }}\n'
-  + '相差金额：¥{{ amountdifference }}\n'
+  + '相差金额：￥{{ amountdifference }}\n'
   + '集资参与人数：{{ backercount }}人\n'
   + '项目截止时间：{{ endtime }}\n'
   + '距离项目截止还有：{{ timedifference }}';
