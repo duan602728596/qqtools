190068fa369a21e3e3db802e0a9939135df666bb
段昊辰 <duanhaochen@126.com>
2018-10-26 00:01:15 +0800

    调整代码分割的代码

--- app/src/modules/Help/Layout.js
+++ app/src/modules/Help/Layout.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import Index from './Index/index';
 
-const ModuleLayout: Function = (props: Object): React.Element=>{
-  return (
-    <Switch>
-      <Route path="/Help" component={ Index } exact={ true } />
-    </Switch>
-  );
-};
+class ModuleLayout extends Component{
+  static propTypes: Object = {
+    injectReducers: PropTypes.func
+  };
+
+  render(): React.Element{
+    return (
+      <Switch>
+        <Route path="/Help" component={ Index } exact={ true } />
+      </Switch>
+    );
+  }
+}
 
 export default ModuleLayout;
--- app/src/modules/Option/Layout.js
+++ app/src/modules/Option/Layout.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
+import reducer from './store/reducer';
 import Index from './Index/index';
 import Edit from './Edit/index';
 
-const ModuleLayout: Function = (props: Object): React.Element=>{
-  return (
-    <Switch>
-      <Route path="/Option" component={ Index } exact={ true } />
-      <Route path="/Option/Edit" component={ Edit } exact={ true } />
-    </Switch>
-  );
-};
+class ModuleLayout extends Component{
+  static propTypes: Object = {
+    injectReducers: PropTypes.func
+  };
+
+  constructor(): void{
+    super(...arguments);
+
+    this.props.injectReducers(reducer);
+  }
+  render(): React.Element{
+    return (
+      <Switch>
+        <Route path="/Option" component={ Index } exact={ true } />
+        <Route path="/Option/Edit" component={ Edit } exact={ true } />
+      </Switch>
+    );
+  }
+}
 
 export default ModuleLayout;
-export reducer from './store/reducer';
