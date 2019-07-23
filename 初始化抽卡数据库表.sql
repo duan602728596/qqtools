0fd9b4165d92d3337084e88f6b19be30b8070576
段昊辰 <duanhaochen@126.com>
2019-07-23 23:25:19 +0800

    feat: 初始化数据库添加积分字段

--- 初始化抽卡数据库表.sql
+++ 初始化抽卡数据库表.sql
@@ -11,6 +11,8 @@ CREATE TABLE `chouka` (
   `nickname` VARCHAR(50) NOT NULL COMMENT '用户昵称',
   -- 卡的信息
   `record` TEXT NOT NULL COMMENT '卡的数量，json格式',
+  -- 积分
+  `points` int(10) unsigned zerofill DEFAULT 0000000000 COMMENT '积分',
   INDEX `id` (`id`)
 )
 COMMENT='抽卡'
