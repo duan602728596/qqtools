ac4197bdfa2b38efeeb97e1a70237b7d9355de5b
段昊辰 <duanhaochen@126.com>
2019-08-27 21:50:50 +0800

    feat: 添加二维码

--- app/src/modules/Index/Index/Navs.js
+++ app/src/modules/Index/Index/Navs.js
@@ -10,7 +10,7 @@ function Navs(props) {
         <dl className={ style.linkGroup }>
           <dt className={ style.bTest }>
             <Link to="/Login" title="QQ登录">
-              <img src={ require('../image/dyx1.webp') } alt="QQ登录" />
+              <img src={ require('../images/dyx1.webp') } alt="QQ登录" />
             </Link>
           </dt>
           <dd>
@@ -22,7 +22,7 @@ function Navs(props) {
         <dl className={ style.linkGroup }>
           <dt className={ style.bTest }>
             <Link to="/Option" title="群功能配置">
-              <img src={ require('../image/lyn1.webp') } alt="群功能配置" />
+              <img src={ require('../images/lyn1.webp') } alt="群功能配置" />
             </Link>
           </dt>
           <dd>
@@ -34,7 +34,7 @@ function Navs(props) {
         <dl className={ style.linkGroup }>
           <dt className={ style.bTest }>
             <Link to="/KouDai48" title="口袋48登录和房间ID查询">
-              <img src={ require('../image/kd481.webp') } alt="口袋48登录和房间ID查询" />
+              <img src={ require('../images/kd481.webp') } alt="口袋48登录和房间ID查询" />
             </Link>
           </dt>
           <dd>
@@ -46,12 +46,28 @@ function Navs(props) {
         <dl className={ style.linkGroup }>
           <dt className={ style.bTest }>
             <Link to="/Help" title="帮助">
-              <img src={ require('../image/pyq1.webp') } alt="帮助" />
+              <img src={ require('../images/pyq1.webp') } alt="帮助" />
             </Link>
           </dt>
           <dd>
             <Link to="/Help">帮助</Link>
           </dd>
+        </dl>
+      </Col>
+      <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
+        <dl className={ style.linkGroup }>
+          <dt className={ style.bTest }>
+            <img className={ style.zfb } src={ require('../images/zfb.webp') } alt="支付宝二维码" />
+          </dt>
+          <dd>欢迎给我打赏</dd>
+        </dl>
+      </Col>
+      <Col xl={ 4 } lg={ 4 } md={ 6 } sm={ 8 } xs={ 12 }>
+        <dl className={ style.linkGroup }>
+          <dt className={ style.bTest }>
+            <img className={ style.wx } src={ require('../images/wx.webp') } alt="微信二维码" />
+          </dt>
+          <dd>欢迎给我打赏</dd>
         </dl>
       </Col>
     </Row>
