4784437e984e94a0ccf7a02362823b93b9203226
段昊辰 <duanhaochen@126.com>
2020-09-03 23:34:38 +0800

    build: 写入信息

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,9 @@
 const util = require('util');
 const glob = require('glob');
 const path = require('path');
+const { promises: fs } = require('fs');
 const fse = require('fs-extra');
+const { version } = require('../www/dist/version.json');
 
 const globPromise = util.promisify(glob);
 
@@ -9,6 +11,10 @@ const cwd = path.join(__dirname, '../');
 const build = path.join(cwd, 'build');
 
 async function clean() {
+  // 写入版本号
+  // await fs.writeFile(path.join(build, 'mac/mac/version'), version);
+  await fs.writeFile(path.join(build, 'win/win-unpacked/version'), version);
+
   // 删除mac
   /*
   const macFiles = await globPromise(path.join(build, 'mac/mac/qqtools.app/Contents/Resources/*.lproj'));
@@ -26,6 +32,10 @@ async function clean() {
   winFiles.forEach((o) => !/zh-CN/i.test(o) && winDeleteTasks.push(fse.remove(o)));
 
   await Promise.all(winDeleteTasks);
+
+  // 重命名
+  // await fs.rename(path.join(build, 'mac/mac'), path.join(build, `mac/qqtools3-${ version }-mac`));
+  await fs.rename(path.join(build, 'win/win-unpacked'), path.join(build, `win/qqtools3-${ version }-winx64`));
 }
 
 clean();
