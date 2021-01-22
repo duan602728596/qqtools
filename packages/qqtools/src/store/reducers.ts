0e5ec20fe0923aaae904731d8822aacda0eee46f
段昊辰 <duanhaochen@126.com>
2021-01-22 23:10:46 +0800

    pref: 修改store的创建

--- packages/qqtools/src/store/reducers.ts
+++ packages/qqtools/src/store/reducers.ts
@@ -1,20 +1,12 @@
-import { combineReducers, ReducersMapObject, Reducer } from '@reduxjs/toolkit';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import loginReducers from '../pages/Login/reducers/reducers';
 import optionsReducers from '../pages/Options/reducers/reducers';
 
 /* reducers */
-const reducers: ReducersMapObject = {
-  ...loginReducers,
-  ...optionsReducers
-};
-
-/* 创建reducer */
-export function createReducer(asyncReducers: ReducersMapObject): Reducer {
-  return combineReducers({
-    ...reducers,
-    ...asyncReducers
-  });
-}
+export const reducersMapObject: ReducersMapObject = Object.assign({},
+  loginReducers,
+  optionsReducers
+);
 
 export const ignoreOptions: any = {
   ignoredPaths: ['login.loginList'],
--- packages/qqtools/src/store/store.ts
+++ packages/qqtools/src/store/store.ts
@@ -1,44 +1,33 @@
 /* 全局的store */
-import { configureStore, getDefaultMiddleware, ReducersMapObject, Reducer, Store, DeepPartial } from '@reduxjs/toolkit';
-import { createReducer, ignoreOptions } from './reducers';
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+  Reducer,
+  Store,
+  DeepPartial
+} from '@reduxjs/toolkit';
+import { reducersMapObject, ignoreOptions } from './reducers';
 
 /* reducer列表 */
-const reducer: Reducer = createReducer({});
-const asyncReducers: ReducersMapObject = {}; // 异步的reducers
+const reducer: Reducer = combineReducers(reducersMapObject);
 
 /* store */
-const store: Store = {} as Store;
+export let store: Store;
 
 export function storeFactory(initialState: DeepPartial<any> = {}): Store {
   // 避免热替换导致redux的状态丢失
-  if (Object.keys(store).length === 0) {
+  if (!store) {
     /* store */
-    Object.assign(store, configureStore({
+    store = configureStore({
       reducer,
       preloadedState: initialState,
       middleware: getDefaultMiddleware({
         immutableCheck: ignoreOptions,
         serializableCheck: ignoreOptions
       })
-    }));
+    });
   }
 
   return store;
 }
-
-/* 注入store */
-export function injectReducers(asyncReducer: ReducersMapObject): void {
-  for (const key in asyncReducer) {
-    // 获取reducer的key值，并将reducer保存起来
-    if (!(key in asyncReducers)) {
-      const item: Reducer = asyncReducer[key];
-
-      asyncReducers[key] = item;
-    }
-  }
-
-  // 异步注入reducer
-  store.replaceReducer(createReducer(asyncReducers));
-}
-
-export default store;
