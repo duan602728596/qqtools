af17403156495fe00b625912fd741c88766e2530
段昊辰 <duanhaochen@126.com>
2021-03-30 11:08:09 +0800

    perf: 优化主线程代码

--- packages/main/src/ipc.ts
+++ packages/main/src/ipc.ts
@@ -1,10 +1,21 @@
 import { ipcMain, BrowserWindow, IpcMainEvent } from 'electron';
 
+const DEVELOP_TOOLS_CHANNEL: string = 'developer-tools';
+
+/* 移除所有监听的通信 */
+const removeListenerChannel: Array<string> = [
+  DEVELOP_TOOLS_CHANNEL
+];
+
+export function removeIpc(): void {
+  for (const channel of removeListenerChannel) {
+    ipcMain.removeAllListeners(channel);
+  }
+}
+
 /* ipc通信 */
-function ipc(win: BrowserWindow): void {
-  ipcMain.on('developer-tools', function(event: IpcMainEvent, ...args: any[]): void {
+export function ipc(win: BrowserWindow): void {
+  ipcMain.on(DEVELOP_TOOLS_CHANNEL, function(event: IpcMainEvent, ...args: any[]): void {
     win.webContents.openDevTools();
   });
 }
-
-export default ipc;
--- packages/main/src/main.ts
+++ packages/main/src/main.ts
@@ -3,13 +3,14 @@ import * as path from 'path';
 import * as url from 'url';
 import { app, BrowserWindow, Menu } from 'electron';
 import { initialize } from '@electron/remote/main';
-import ipc from './ipc';
-
-const isDevelopment: boolean = process.env.NODE_ENV === 'development';
-let win: BrowserWindow | null = null;
+import { isDevelopment } from './utils';
+import { ipc, removeIpc } from './ipc';
 
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1'; // 关闭警告
 initialize();
+
+/* BrowserWindow窗口对象 */
+let win: BrowserWindow | null = null;
 
 /* 初始化 */
 function createWindow(): void {
@@ -43,6 +44,7 @@ function createWindow(): void {
   ipc(win);
 
   win.on('closed', function(): void {
+    removeIpc();
     win = null;
   });
 }
