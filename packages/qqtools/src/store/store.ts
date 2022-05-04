b3f7f786f396c2ce6a6b9a5ab0e21629e8cbf5c5
段昊辰 <duanhaochen@126.com>
2022-05-04 18:10:42 +0800

    fix: 修改类型错误

--- packages/qqtools/src/store/store.ts
+++ packages/qqtools/src/store/store.ts
@@ -7,10 +7,9 @@ import {
   type DeepPartial,
   type ImmutableStateInvariantMiddlewareOptions,
   type SerializableStateInvariantMiddlewareOptions,
-  type MiddlewareArray,
   type Middleware
 } from '@reduxjs/toolkit';
-import type { CurriedGetDefaultMiddleware, ThunkMiddlewareFor } from '@reduxjs/toolkit/src/getDefaultMiddleware';
+import type { CurriedGetDefaultMiddleware } from '@reduxjs/toolkit/src/getDefaultMiddleware';
 import { reducersMapObject, ignoreOptions } from './reducers';
 
 interface ThunkOptions<E = any> {
@@ -23,31 +22,28 @@ interface GetDefaultMiddlewareOptions {
   serializableCheck?: boolean | SerializableStateInvariantMiddlewareOptions;
 }
 
-type MiddlewareCbReturn = MiddlewareArray<
-  Middleware<{}, any>
-  | ThunkMiddlewareFor<any, GetDefaultMiddlewareOptions>
->;
-
 /* reducer列表 */
 const reducer: Reducer = combineReducers(reducersMapObject);
 
 /* store */
 export let store: Store;
 
+function createStore(initialState: DeepPartial<any> = {}): void {
+  store = configureStore({
+    reducer,
+    preloadedState: initialState,
+    middleware(getDefaultMiddleware: CurriedGetDefaultMiddleware): ReadonlyArray<Middleware> {
+      return getDefaultMiddleware<GetDefaultMiddlewareOptions>({
+        immutableCheck: ignoreOptions,
+        serializableCheck: ignoreOptions
+      });
+    }
+  });
+}
+
 export function storeFactory(initialState: DeepPartial<any> = {}): Store {
-  // 避免热替换导致redux的状态丢失
   if (!store) {
-    /* store */
-    store = configureStore({
-      reducer,
-      preloadedState: initialState,
-      middleware(getDefaultMiddleware: CurriedGetDefaultMiddleware): MiddlewareCbReturn {
-        return getDefaultMiddleware<GetDefaultMiddlewareOptions>({
-          immutableCheck: ignoreOptions,
-          serializableCheck: ignoreOptions
-        });
-      }
-    });
+    createStore(initialState);
   }
 
   return store;
