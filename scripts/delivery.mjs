e152a2eb78572c35f0b5eaa25a6fbefb449a23b3
段昊辰 <duanhaochen@126.com>
2022-07-17 23:07:57 +0800

    fix: 修复错误

--- scripts/delivery.mjs
+++ scripts/delivery.mjs
@@ -5,8 +5,8 @@ import { cwd, command, isWindows } from './utils.mjs';
 async function delivery() {
   const npm = isWindows ? 'npm.cmd' : 'npm';
 
-  await command(isWindows, ['run', 'build'], path.join(cwd, 'packages/main'));
-  await command(isWindows, ['run', 'build'], path.join(cwd, 'packages/qqtools'));
+  await command(npm, ['run', 'build'], path.join(cwd, 'packages/main'));
+  await command(npm, ['run', 'build'], path.join(cwd, 'packages/qqtools'));
   await command('node', ['--experimental-json-modules', './scripts/unpack.mjs'], cwd);
   await command('node', ['--experimental-json-modules', './scripts/clean.mjs'], cwd);
 }
