a083f3d40f51aedfaf96ab2704a381d7ba636b2f
段昊辰 <duanhaochen@126.com>
2022-07-23 23:10:52 +0800

    refactor: 添加v2类型

--- packages/qqtools/src/types.d.ts
+++ packages/qqtools/src/types.d.ts
@@ -4,8 +4,8 @@ export interface EditItem {
   value: string;
 }
 
-// 表单内的值
-export interface OptionsItemValue {
+// 一些基础的配置
+export interface OptionsItemBasic {
   optionName: string;
   optionType?: '0' | '1'; // 0: mirai-api-http，1: oicq
   qqNumber: number;
@@ -13,7 +13,15 @@ export interface OptionsItemValue {
   socketHost?: string;
   socketPort: number;
   authKey: string;
+  // 群欢迎
+  groupWelcome?: boolean;
+  groupWelcomeSend?: string;
+  // 自定义命令
+  customCmd?: Array<EditItem>;
+}
 
+// 口袋48
+export interface OptionsItemPocket48 {
   pocket48RoomListener?: boolean;
   pocket48RoomId?: string;
   pocket48IsAnonymous?: boolean;
@@ -26,29 +34,46 @@ export interface OptionsItemValue {
   pocket48MemberInfo?: boolean;
   pocket48LogSave?: boolean;
   pocket48LogDir?: string;
+}
 
+// 微博
+export interface OptionsItemWeibo {
   weiboListener?: boolean;
   weiboUid?: string;
   weiboAtAll?: string;
   weiboSuperTopicListener?: boolean;
   weiboSuperTopicLfid?: string;
+}
 
+// bilibili
+export interface OptionsItemBilibili {
   bilibiliLive?: boolean;
   bilibiliLiveId: string;
   bilibiliAtAll?: string;
+}
 
-  taobaListen?: boolean;
-  taobaId?: string;
-  taobaCommandTemplate?: string;
-
-  groupWelcome?: boolean;
-  groupWelcomeSend?: string;
-
+// 定时任务
+export interface OptionsItemCronTimer {
   cronJob?: boolean;
   cronTime?: string;
   cronSendData?: string;
+}
 
-  customCmd?: Array<EditItem>;
+// 表单内的值
+export interface OptionsItemValue extends
+  OptionsItemBasic,
+  OptionsItemPocket48,
+  OptionsItemWeibo,
+  OptionsItemBilibili,
+  OptionsItemCronTimer {}
+
+// 表单内的值v2，将部分字段转换成数组，从而支持多个配置
+export interface OptionsItemValueV2 extends OptionsItemBasic {
+  version: 'v2';
+  pocket48?: Array<OptionsItemPocket48>;
+  weibo?: Array<OptionsItemWeibo>;
+  bilibili?: Array<OptionsItemBilibili>;
+  cronTimer?: Array<OptionsItemCronTimer>;
 }
 
 // 配置
