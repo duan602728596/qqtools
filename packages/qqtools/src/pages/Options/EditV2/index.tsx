93fdcecaa4acfef4a08d643f059cfe207cfe7531
段昊辰 <duanhaochen@126.com>
2022-07-24 15:39:46 +0800

    refactor: 数据的兼容处理

--- packages/qqtools/src/pages/Options/EditV2/index.tsx
+++ packages/qqtools/src/pages/Options/EditV2/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, type ReactElement, type MouseEvent, type RefObject }
 import { useDispatch } from 'react-redux';
 import type { Dispatch } from '@reduxjs/toolkit';
 import { useNavigate, useParams, type NavigateFunction, type Params } from 'react-router-dom';
-import SchemaForm from 'antd-schema-form';
+import SchemaForm, { getObjectFromValue } from 'antd-schema-form';
 import 'antd-schema-form/style/antd-schema-form.css';
 import type { FormInstance } from 'antd';
 import type { Store } from 'rc-field-form/es/interface';
@@ -12,8 +12,9 @@ import pocket48LogDir from './components/pocket48LogDir';
 import IMLogin from './components/IMLogin';
 import randomId from './components/randomId';
 import { getOptionItem, saveFormData } from '../reducers/reducers';
+import { pick } from '../../../utils/lodash';
 import * as editV2SchemaJson from './editv2.schema.json' assert { type: 'json' };
-import type { OptionsItem } from '../../../types';
+import type { OptionsItem, OptionsItemValue, OptionsItemValueV2 } from '../../../types';
 
 const customComponent: Record<string, Function> = {
   checkboxDesc,
@@ -31,12 +32,69 @@ function EditV2(props: {}): ReactElement {
 
   // 数据回填
   async function getData(): Promise<void> {
-    if (params?.id && formRef.current) {
+    if (params?.id) {
       const { result }: { result: OptionsItem } = await dispatch(getOptionItem({
         query: params.id
       }));
+      let value: OptionsItemValue | OptionsItemValueV2 = result.value;
 
-      formRef.current.setFieldsValue({ $root: result.value });
+      // 数据处理，保证数据的格式
+      if (!('version' in result.value)) {
+        const formatValue: OptionsItemValueV2 = pick(result.value, [
+          'optionName',
+          'optionType',
+          'qqNumber',
+          'groupNumber',
+          'socketHost',
+          'socketPort',
+          'authKey',
+          'groupWelcome',
+          'groupWelcomeSend',
+          'customCmd'
+        ]);
+        const [pocket48, weibo, bilibili, cronTimer]: Array<Pick<OptionsItemValue, keyof OptionsItemValue>> = [
+          pick(result.value, [
+            'pocket48RoomListener',
+            'pocket48RoomId',
+            'pocket48IsAnonymous',
+            'pocket48Account',
+            'pocket48Token',
+            'pocket48LiveAtAll',
+            'pocket48ShieldMsgType',
+            'pocket48RoomEntryListener',
+            'pocket48OwnerOnlineListener',
+            'pocket48MemberInfo',
+            'pocket48LogSave',
+            'pocket48LogDir'
+          ]),
+          pick(result.value, [
+            'weiboListener',
+            'weiboUid',
+            'weiboAtAll',
+            'weiboSuperTopicListener',
+            'weiboSuperTopicLfid'
+          ]),
+          pick(result.value, [
+            'bilibiliLive',
+            'bilibiliLiveId',
+            'bilibiliAtAll'
+          ]),
+          pick(result.value, [
+            'cronJob',
+            'cronTime',
+            'cronSendData'
+          ])
+        ];
+
+        Object.values(pocket48).some((o: any): boolean => o !== undefined) && (formatValue.pocket48 = [pocket48]);
+        Object.values(weibo).some((o: any): boolean => o !== undefined) && (formatValue.weibo = [weibo]);
+        Object.values(bilibili).some((o: any): boolean => o !== undefined) && (formatValue.bilibili = [bilibili]);
+        Object.values(cronTimer).some((o: any): boolean => o !== undefined) && (formatValue.cronTimer = [cronTimer]);
+        value = formatValue;
+        value.version = 'v2';
+      }
+
+      formRef.current && formRef.current.setFieldsValue(getObjectFromValue({ $root: value }));
     }
   }
 
