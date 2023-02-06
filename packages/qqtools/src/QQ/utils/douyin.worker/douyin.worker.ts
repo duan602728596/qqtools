c05efd20cadc87e8049c714ac1eaf1ea886fb3cf
Haochen Duan <haochenduan@microsoft.com>
2023-02-06 16:57:02 +0800

    perf: 优化抖音监听代码

--- packages/qqtools/src/QQ/utils/douyin.worker/douyin.worker.ts
+++ packages/qqtools/src/QQ/utils/douyin.worker/douyin.worker.ts
@@ -12,16 +12,18 @@ import type * as oicq from 'oicq';
 import { plain } from '../miraiUtils';
 import type { UserScriptRendedData, UserItem1, UserItem2, UserDataItem, MessageChain } from '../../qq.types';
 
-let browser: Browser | undefined = undefined;
-let context: ChromiumBrowserContext | undefined = undefined;
-let page: Page | undefined = undefined;
-let closePageTimer: NodeJS.Timer | undefined = undefined;
-let userId: string;
-let protocol: string; // 协议：mirai或者oicq
-let id: string; // 记录查询位置
-let nickname: string | undefined;
-let douyinTimer: NodeJS.Timer | undefined = undefined;
-let executablePath: string;
+/* playwright相关 */
+let browser: Browser | undefined = undefined;                // 浏览器browser
+let context: ChromiumBrowserContext | undefined = undefined; // 浏览器上下文context
+let page: Page | undefined = undefined;                      // 浏览器页面page
+let closePageTimer: NodeJS.Timer | undefined = undefined;    // 监听并关闭page
+
+/* 抖音 */
+let userId: string;                                    // 用户userId
+let protocol: string;                                  // 协议：mirai或者oicq
+let id: string | '0' | null = null;                    // 记录查询位置，为0表示当前没有数据，和null不同，null表示请求数据失败了
+let douyinTimer: NodeJS.Timer | undefined = undefined; // 轮询定时器
+let executablePath: string;                            // 浏览器路径
 
 interface DouyinSendMsg {
   url: string;
@@ -83,7 +85,7 @@ async function getDouyinData(): Promise<UserScriptRendedData | void> {
         clearTimeout(closePageTimer);
         closePageTimer = undefined;
       }
-    }, 300_000);
+    }, 90_000);
 
     const renderDataHandle: JSHandle = await page.evaluateHandle(
       (): string => document.getElementById('RENDER_DATA')!.innerHTML);
@@ -110,17 +112,20 @@ async function handleDouyinListener(): Promise<void> {
   try {
     const renderData: UserScriptRendedData | void = await getDouyinData();
 
-    if (!renderData) return;
+    if (!renderData) return console.warn('没有获取到RENDER_DATA。', userId);
 
-    const userItemArray: Array<UserItem1 | UserItem2 | string> = Object.values(renderData);
-    const userItem2: UserItem2 | undefined = userItemArray.find(
-      (o: UserItem1 | UserItem2 | string): o is UserItem2 => typeof o === 'object' && ('post' in o));
+    const userItemArray: Array<UserItem1 | UserItem2> = Object.values(renderData);
+    const userItem2: UserItem2 | undefined = userItemArray.find((o: UserItem1 | UserItem2): o is UserItem2 => typeof o === 'object' && ('post' in o));
 
     if (userItem2) {
       const data: Array<UserDataItem> = userItem2.post.data.sort(
         (a: UserDataItem, b: UserDataItem) => b.createTime - a.createTime);
 
-      if (data.length && data[0].awemeId !== id) {
+      if (id === null) {
+        id = data.length ? data[0].awemeId : '0';
+      }
+
+      if (data.length && data[0].awemeId !== id && id !== null) {
         const sendData: DouyinSendMsg = {
           url: `https:${ data[0].video.playApi }`,
           time: dayjs.unix(data[0].createTime).format('YYYY-MM-DD HH:mm:ss'),
@@ -138,7 +143,7 @@ async function handleDouyinListener(): Promise<void> {
     console.error(err);
   }
 
-  douyinTimer = setTimeout(handleDouyinListener, 150_000);
+  douyinTimer = setTimeout(handleDouyinListener, 180_000);
 }
 
 /* 初始化获取抖音的记录位置 */
@@ -146,24 +151,22 @@ async function douyinInit(): Promise<void> {
   try {
     const renderData: UserScriptRendedData | void = await getDouyinData();
 
-    if (!renderData) return;
+    if (!renderData) return console.warn('初始化时没有获取到RENDER_DATA。', userId);
 
-    const userItemArray: Array<UserItem1 | UserItem2 | string> = Object.values(renderData);
-    const userItem2: UserItem2 | undefined = userItemArray.find(
-      (o: UserItem1 | UserItem2 | string): o is UserItem2 => typeof o === 'object' && ('post' in o));
+    const userItemArray: Array<UserItem1 | UserItem2> = Object.values(renderData);
+    const userItem2: UserItem2 | undefined = userItemArray.find((o: UserItem1 | UserItem2): o is UserItem2 => typeof o === 'object' && ('post' in o));
 
     if (userItem2) {
-      nickname = userItem2.user.user.nickname;
-
       const data: Array<UserDataItem> = userItem2.post.data.sort(
         (a: UserDataItem, b: UserDataItem) => b.createTime - a.createTime);
 
       id = data.length ? data[0].awemeId : '0';
-      douyinTimer = setTimeout(handleDouyinListener, 150_000);
     }
   } catch (err) {
     console.error(err);
   }
+
+  douyinTimer = setTimeout(handleDouyinListener, 180_000);
 }
 
 addEventListener('message', function(event: MessageEvent) {
