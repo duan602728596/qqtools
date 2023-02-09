59e55d01ad061f1f42364f2ade3aa74b0f62395a
段昊辰 <duanhaochen@126.com>
2023-02-09 23:25:39 +0800

    perf: 添加错误捕捉

--- packages/main/src/main.ts
+++ packages/main/src/main.ts
@@ -39,7 +39,10 @@ function createWindow(): void {
   Menu.setApplicationMenu(null);
 
   ipc(win);
-  ipcRemoteHandle(win);
+
+  try {
+    ipcRemoteHandle(win);
+  } catch {}
 
   win.on('closed', function(): void {
     removeIpc();
