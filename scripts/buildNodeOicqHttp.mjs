e8072c18aa487523312c4ebf0d40e3ad7076baf6
段昊辰 <duanhaochen@126.com>
2023-02-23 20:43:19 +0800

    build: 移除arm64的构建

--- scripts/buildNodeOicqHttp.mjs
+++ scripts/buildNodeOicqHttp.mjs
@@ -25,12 +25,8 @@ async function buildNodeOicqHttp() {
     {
       target: 'node18-macos-x64',
       name: 'node-oicqhttp-macos'
-    },
-    isMacOS && {
-      target: 'node18-macos-arm64',
-      name: 'node-oicqhttp-macos-arm64'
     }
-  ].filter(Boolean);
+  ];
 
   // 编译
   await rimraf(nodeOicqHttpBuild);
@@ -41,6 +37,7 @@ async function buildNodeOicqHttp() {
     const outputPath = path.join(nodeOicqHttpBuild, option.name);
 
     await exec([
+      '--debug',
       '--target',
       option.target,
       '--output',
