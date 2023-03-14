bf8ce021988329941455ca48352ec0b0f8b86e94
告白，花，狼 <duanhaochen@126.com>
2023-03-14 15:16:00 +0800

    fix: 添加缺少的空格

--- packages/qqtools/src/QQ/services/douyin.ts
+++ packages/qqtools/src/QQ/services/douyin.ts
@@ -8,7 +8,7 @@ import type { AwemePostResponse } from './interface';
  */
 
 const pcUserAgent: string = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36'
-  + '(KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69';
+  + ' (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69';
 
 /* 请求ttwid */
 export async function requestTtwidCookie(): Promise<string | undefined> {
