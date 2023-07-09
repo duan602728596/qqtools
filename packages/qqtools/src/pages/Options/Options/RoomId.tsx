828841d3d4afc2e1d163b68eaa807a7f5aef0588
段昊辰 <duanhaochen@126.com>
2023-07-10 00:09:37 +0800

    feat: 通过请求API可以导入房间信息

--- packages/qqtools/src/pages/Options/Options/RoomId.tsx
+++ packages/qqtools/src/pages/Options/Options/RoomId.tsx
@@ -7,17 +7,46 @@ import { Button, Modal, Space, Alert, message } from 'antd';
 import type { UseMessageReturnType } from '@qqtools-types/antd';
 import { showOpenDialog } from '../../../utils/remote/dialog';
 import { saveRoomId, deleteRoomId } from '../reducers/options';
+import { requestRoomId } from '../services/jsdelivrCDN';
 import type { MemberInfo } from '../../../commonTypes';
+import type { RoomIdObj } from '../services/interface';
 
 /* 房间信息导入 */
 function RoomId(props: {}): ReactElement {
   const dispatch: Dispatch = useDispatch();
   const [messageApi, messageContextHolder]: UseMessageReturnType = message.useMessage();
   const [visible, setVisible]: [boolean, D<S<boolean>>] = useState(false);
+  const [loading, setLoading]: [boolean, D<S<boolean>>] = useState(false);
 
   // 下载列表文件
   function handleDownloadRoomIdJsonClick(event: MouseEvent<HTMLAnchorElement>): void {
     shell.openExternal('https://raw.githubusercontent.com/duan602728596/qqtools/main/packages/NIMTest/node/roomId.json');
+  }
+
+  // 通过api导入文件
+  async function handleImportRoomIdJsonFromAPIClick(event: MouseEvent): Promise<void> {
+    setLoading(true);
+
+    try {
+      const res: RoomIdObj = await requestRoomId();
+
+      if (res?.roomId?.length) {
+        dispatch(saveRoomId({
+          data: {
+            name: 'roomId',
+            value: res.roomId
+          }
+        }));
+        messageApi.success('房间信息导入成功！');
+      } else {
+        messageApi.error('房间信息导入失败！');
+      }
+    } catch (err) {
+      console.error(err);
+      messageApi.error('房间信息导入失败！');
+    }
+
+    setLoading(false);
   }
 
   // 导入文件
@@ -58,19 +87,23 @@ function RoomId(props: {}): ReactElement {
     <Fragment>
       <Button onClick={ (event: MouseEvent): void => setVisible(true) }>导入房间信息</Button>
       <Modal open={ visible }
-        width={ 500 }
+        width={ 600 }
         centered={ true }
+        maskClosable={ false }
         onCancel={ (event: MouseEvent): void => setVisible(false) }
       >
-        <div className="h-[110px]">
+        <div className="h-[160px]">
           <Space className="mb-[16px]">
-            <Button onClick={ handleImportRoomIdJsonClick }>导入房间信息</Button>
+            <Button onClick={ handleImportRoomIdJsonClick }>本地JSON文件导入房间信息</Button>
+            <Button loading={ loading } onClick={ handleImportRoomIdJsonFromAPIClick }>API导入房间信息</Button>
             <Button type="primary" danger={ true } onClick={ handleDeleteRoomIdJsonClick }>清除房间信息</Button>
           </Space>
           <Alert type="info" message={ [
             '如果想要监听房间出入信息，必须先导入房间的信息列表。',
             <a key="download" role="button" aria-label="点击下载" onClick={ handleDownloadRoomIdJsonClick }>点击下载</a>,
-            '列表文件。'
+            '列表文件。',
+            <br key="br" />,
+            '或者直接通过请求API导入房间信息。'
           ] } />
         </div>
       </Modal>
