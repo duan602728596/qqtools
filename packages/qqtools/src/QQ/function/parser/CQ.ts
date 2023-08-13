202d618959ab09d94520db305f66773fd8162b5d
段昊辰 <duanhaochen@126.com>
2023-08-13 23:59:32 +0800

    perf: 在at后面添加空格
    re #161

--- packages/qqtools/src/QQ/function/parser/CQ.ts
+++ packages/qqtools/src/QQ/function/parser/CQ.ts
@@ -7,11 +7,11 @@ export function image(file: string, type: 'file' | 'url' = 'file'): string {
 }
 
 export function at(id: number): string {
-  return `[CQ:at,qq=${ id }]`;
+  return `[CQ:at,qq=${ id }] `;
 }
 
 export function atAll(): string {
-  return '[CQ:at,qq=all]';
+  return '[CQ:at,qq=all] ';
 }
 
 export function record(file: string, type: 'file' | 'url' = 'file'): string {
